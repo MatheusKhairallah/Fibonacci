c9e2f6ba830f5da5275271485f647fa4ecc9ef6a
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-07-15 10:21:38 -0300

    Add files via upload
    Aprimoramento do programa

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro (> =  2): ");
+            int numero=Convert.ToInt32(Console.ReadLine());
+
+            int f1=0;
+            int f2=1;
+            int soma=0;
+
+            if (numero<2)
+            {
+                Console.WriteLine("Por favor digite um número maior ou igual a 2");
+                Environment.Exit(-1);
+            }
+            
+            for (int i = 0; i <numero; i++)
+            {
+                Console.Write(soma + " ");
+                soma=f1 + f2;
+                f2=f1;
+                f1=soma;
+            }
+
+        }
+    }
+}
